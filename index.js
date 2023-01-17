f8fab15d3767f08d6b92b2ecce4df51425b94edc
AndrewDobriy <an.dobriy@mail.ru>
2023-01-17 15:01:33 +0300

    Added Russian sity and search

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const search = document.querySelector('#search');
 const matchList = document.querySelector('#match-list');
 
-// Search states.json and filter it
+// Search russian-cities.json and filter it
 const searchStates = async (searchText) => {
-  const res = await fetch('states.json');
+  const res = await fetch('russian-cities.json');
   const states = await res.json();
 
   // Get matches to current text input
   let matches = states.filter((state) => {
     const regex = new RegExp(`^${searchText}`, 'gi');
-    return state.name.match(regex) || state.abbr.match(regex);
+    return state.name.match(regex) || state.district.match(regex);
   });
   if (searchText.length === 0) {
     matches = [];
+    matchList.innerHTML = '';
   }
+
   outPutHtml(matches);
 };
 
 // Show result in HTML
 const outPutHtml = (matches) => {
   if (matches.length > 0) {
-    const html = matches.map((match) => {
-      `<div class='card card-body mb-1'>
-        <h4>${match.name} (${match.abbr}) <span class='text-primary'>${match.capital}</span></h4>
-        <small>Lat: ${match.lat} / Long: ${match.long}</small>
-        </div>`;
-    });
-    console.log(html);
+    const html = matches
+      .map((match) => {
+        return `<div class='card card-body mb-1'>
+        <h4>${match.name} (${match.district} Федеральный округ)
+          <div>
+            <span class='text-primary'>Население: ${match.population}</span>
+          </div>
+        </h4>
+        <small>Lat ${match.coords.lat} / Long ${match.coords.lon}</small>
+      </div>`;
+      })
+      .join('');
 
     matchList.innerHTML = html;
   }
